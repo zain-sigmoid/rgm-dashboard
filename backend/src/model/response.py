d0664fd43ebfd04318d849e58a95ca450cd99edc
Mohd Zain <zainmohd@sigmoidanalytics.com>
2025-11-26 00:37:22 +0530

    add : model

---
+++ backend/src/model/response.py
@@ -0,0 +1,11 @@
+"""
+This file contains models used across backend
+"""
+
+from pydantic import BaseModel
+from typing import Optional, Any
+
+
+class Response(BaseModel):
+    # to be implemented
+    raise NotImplemented
