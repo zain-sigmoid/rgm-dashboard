1f690e899d41016271e5d58b57bb019ebc930fdd
Mohd Zain <zainmohd@sigmoidanalytics.com>
2025-11-26 00:38:11 +0530

    removed not required content

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Toast from "./components/Toast";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import Scrolltotop from "./components/Scrolltotop";
+import Wrong from "./pages/Wrong";
+import About from "./pages/About";
 
-function App() {
-  const [count, setCount] = useState(0)
+const AppShell = () => {
+  const host = "http://127.0.0.1:8000/api";
+  const [alert, setAlert] = useState(null);
+  const [toast, setToast] = useState(null);
+  const location = useLocation();
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3500);
+  };
+
+  const showToast = (content, copy = false, msg = "", variant = "primary") => {
+    console.log(content, copy, msg);
+    setToast({
+      content,
+      copy,
+      msg,
+      variant,
+    });
+  };
+
+  const handleToastClose = () => {
+    setToast(null);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Scrolltotop />
+      <Alert alert={alert} />
+      {toast && (
+        <Toast
+          content={toast.content}
+          copy={toast.copy}
+          msg={toast.msg}
+          variant={toast.variant}
+          onClose={handleToastClose}
+        />
+      )}
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route
+          exact
+          path="/:wrong"
+          element={<Wrong prop={{ showAlert, showToast }} />}
+        ></Route>
+        <Route
+          exact
+          path="/about"
+          element={<About prop={{ showAlert }} />}
+        ></Route>
+      </Routes>
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+    <Router>
+      <AppShell />
+    </Router>
+  );
 }
 
-export default App
+export default App;
